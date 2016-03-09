ffb46520edf60fbb6d4ef3d844080c24bb7502c0
Jitin Dodd <j2dodd@uwaterloo.ca>
2016-03-08 22:57:55 -0500

    CSRF Token api fix.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,7 @@ class ApiController < ApplicationController
 	http_basic_authenticate_with email:ENV["API_AUTH_EMAIL"], :only => [:signup, :signin, :get_token]  
   	#make sure any request that is not a signup, signin, or get_token has it's authtoken checked
   	before_filter :check_for_valid_authtoken, :except => [:signup, :signin, :get_token]	
-  	
+
 	def signin
 	  if request.post?
 	    if params && params[:email]
@@ -25,6 +25,7 @@ class ApiController < ApplicationController
 	      render :json => e.to_json, :status => 400
 	    end
 	  end
+	  render json: {status: :ok}
 	end
 
 	def get_token

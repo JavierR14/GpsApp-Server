f425e9aee7f6ff46e01d6de8a37e208d4d5b97e2
Brent Brightling <brentwbrightling@gmail.com>
2016-03-20 22:26:29 -0400

    Added basic acceptance of posting location

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 	root 'test#index'
 	get 'test/index'
-	
+
 	post 'api/signup' #=> 'api#signup'
 	post 'api/signin' #=> 'api#signin'
 
 	get 'api/get_token'
 	get 'api/clear_token'
+
+	post 'api/post_location'
 
 	match "*path", to: "application#page_not_found_found", via: :all
 end
